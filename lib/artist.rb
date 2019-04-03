047defcc1fa76fdd9778ff1367eb94865d46e71b
Nate Tan <nktan93@gmail.com>
2019-04-03 19:40:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,16 +9,16 @@ class Artist
   end
   
   def add_song(song)
+    song.artist = self
     @songs << song
     @@songs << song
-    song.artist = self
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
+    song.artist = self
     @songs << song
     @@songs << song
-    song.artist = self
   end
   
   def self.song_count
